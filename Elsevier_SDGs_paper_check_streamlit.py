6b8cbb581124c1f52a0d490195f79009ca23cf7d
cplooo <cplo@kimo.com>
2023-09-30 02:30:20 +0800

    Update Elsevier_SDGs_paper_check_streamlit.py

--- Elsevier_SDGs_paper_check_streamlit.py
+++ Elsevier_SDGs_paper_check_streamlit.py
@@ -3099,9 +3099,9 @@ if SDGs_choice == '17':
     
     
     if cond1_final or cond2_final or cond3_final or cond6_final or cond7_final or cond9_final or cond10_final or cond12_final:
-        st.write(f'查核結果: 恭喜您 ! 你的論文被收錄在 Elsevier SDGs {SDGs_choice}.')
+        st.write(f'查核結果: 恭喜您 ! 你的論文可以被收錄在 Elsevier SDGs {SDGs_choice}.')
     else:
-        st.write(f'查核結果: 很遺憾 ! 你的論文沒有被收錄在 Elsevier SDGs {SDGs_choice}.')
+        st.write(f'查核結果: 很遺憾 ! 你的論文無法被收錄在 Elsevier SDGs {SDGs_choice}.')
 
     with st.expander(f"SDGs {SDGs_choice} 重要關鍵字, 添加這些關鍵字於 title, abstract, keywords 將有助於論文被收錄於 SDGs {SDGs_choice}. 完整收錄條件如下連結 https://tinyurl.com/26xxakca."):
         #st.title("SDGs 17 關鍵字")
